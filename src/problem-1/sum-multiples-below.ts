7d2901205102eafb56122637afe2bfeca5f01296
Augustus Murphy <gusbmurphy@gmail.com>
2023-06-03 00:14:35 -0500

    Refactor to be smarter

--- src/problem-1/sum-multiples-below.ts
+++ src/problem-1/sum-multiples-below.ts
@@ -1,8 +1,20 @@
 function sumMultiplesOf3And5Below (limit: number): number {
-  if (limit === 10) return 23
-  if (limit === 4) return 3
-  if (limit === 8) return 8
-  else return 0
+  const multiplesOf3Or5: number[] = []
+
+  for (let numberToCheck = 3; numberToCheck < limit; numberToCheck++) {
+    if (numberToCheck % 3 === 0 || numberToCheck % 5 === 0) {
+      multiplesOf3Or5.push(numberToCheck)
+    }
+  }
+
+  if (multiplesOf3Or5.length < 1) return 0
+
+  const sumOfMultiples = multiplesOf3Or5.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  )
+
+  return sumOfMultiples
 }
 
 export default sumMultiplesOf3And5Below
