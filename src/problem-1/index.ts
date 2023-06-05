0e0549c1d14d13b10e5f2ede8105f25e86c08d88
Augustus Murphy <gusbmurphy@gmail.com>
2023-06-04 21:11:34 -0500

    Use main funciton in problem-1 index

--- src/problem-1/index.ts
+++ src/problem-1/index.ts
@@ -4,8 +4,16 @@ import sumMultiplesOf3And5Below from './sum-multiples-below'
 
 const rl = readline.createInterface({ input, output })
 
-const givenLimit = await rl.question('Please give an upper limit to sum all multiples of 3 or 5 until.')
+async function main (): Promise<void> {
+  const givenLimit = await rl.question(
+    'Please give an upper limit to sum all multiples of 3 or 5 up to: '
+  )
 
-const sum = sumMultiplesOf3And5Below(parseInt(givenLimit))
+  const sum = sumMultiplesOf3And5Below(parseInt(givenLimit))
 
-console.log(`Sum of all multiples and 3 or 5 below ${givenLimit}: ${sum}`)
+  console.log(`Sum of all multiples and 3 or 5 below ${givenLimit}: ${sum}`)
+
+  rl.close()
+}
+
+main().catch(() => console.error('Something went wrong!'))
