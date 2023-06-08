b30447d3399c2a4bf668350edaf25edd4659be33
Augustus Murphy <gusbmurphy@gmail.com>
2023-06-07 22:08:54 -0500

    Create generateTriangleNumber and implement result for 1

---
+++ src/problem-12/generate-triangle-number.test.ts
@@ -0,0 +1,5 @@
+import generateTriangleNumber from "./generate-triangle-number";
+
+it("returns 1 as the first triangle number", () => {
+  expect(generateTriangleNumber(1)).toBe(1);
+});
---
+++ src/problem-12/generate-triangle-number.ts
@@ -0,0 +1,5 @@
+function generateTriangleNumber(triangleNumberPosition: number): number {
+  return 1;
+}
+
+export default generateTriangleNumber;
