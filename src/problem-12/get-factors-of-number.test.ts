be640d8ef9da927028d76669f8c3304ea0d9746e
Augustus Murphy <gusbmurphy@gmail.com>
2023-06-07 22:22:32 -0500

    Implement factors for 3

--- src/problem-12/get-factors-of-number.test.ts
+++ src/problem-12/get-factors-of-number.test.ts
@@ -7,3 +7,7 @@ it("returns just 1 for an input of 1", () => {
 it("returns 1 and 2 for an input of 2", () => {
   expect(getFactorsOfNumber(2)).toEqual([1, 2]);
 });
+
+it("returns 1 and 3 for an input of 3", () => {
+  expect(getFactorsOfNumber(3)).toEqual([1, 3]);
+});
--- src/problem-12/get-factors-of-number.ts
+++ src/problem-12/get-factors-of-number.ts
@@ -2,7 +2,9 @@ function getFactorsOfNumber(givenNumber: number): number[] {
   const factors: number[] = [];
 
   for (let i = 1; i <= givenNumber; i++) {
-    factors.push(i);
+    if (i == 1 || i == givenNumber) {
+      factors.push(i);
+    }
   }
 
   return factors;
